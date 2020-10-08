573c4be0436b51bd6fa5925e97137a449ecb8f7e
josep <josep@LAPTOP-QTSPQIGD.myfiosgateway.com>
2020-10-07 23:46:49 -0400

    Entrega Tarea 1

---
+++ src/variableexamples/VariableExamples.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variableexamples;
+
+/**
+ *
+ * @author josep
+ */
+public class VariableExamples {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      short sixteenBit = Short.MAX_VALUE;
+      System.out.println(sixteenBit);
+      long sixtyFourBit = 9223372036854775807L;
+      System.out.println(sixtyFourBit);
+      double decimals = 165.4665;
+      System.out.println(decimals);
+    }
+    
+}
